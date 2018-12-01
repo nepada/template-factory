7bc564792f1b342761cbbf1d172674668a7f064d
Petr Morávek <petr@pada.cz>
2018-12-01 21:13:08 +0100

    TemplateFactoryExtension: simplify code

--- src/Bridges/TemplateFactoryDI/TemplateFactoryExtension.php
+++ src/Bridges/TemplateFactoryDI/TemplateFactoryExtension.php
@@ -24,17 +24,19 @@ class TemplateFactoryExtension extends Nette\DI\CompilerExtension
         $container->addDefinition($this->prefix('templateConfigurator'))
             ->setClass(TemplateFactory\TemplateConfigurator::class)
             ->addSetup('setTranslator');
-
-        $templateFactory = $this->getNetteTemplateFactory();
-        $templateFactory->addSetup(
-            '?->onCreate[] = function (Nette\Application\UI\ITemplate $template): void {?->configure($template);}',
-            ['@self', $this->prefix('@templateConfigurator')]
-        );
     }
 
     public function beforeCompile(): void
     {
-        $templateConfigurator = $this->getContainerBuilder()->getDefinition($this->prefix('templateConfigurator'));
+        $container = $this->getContainerBuilder();
+
+        $templateFactory = $container->getDefinitionByType(Nette\Application\UI\ITemplateFactory::class);
+        $templateFactory->addSetup(
+            '?->onCreate[] = function (Nette\Application\UI\ITemplate $template): void {?->configure($template);}',
+            ['@self', $this->prefix('@templateConfigurator')]
+        );
+
+        $templateConfigurator = $container->getDefinition($this->prefix('templateConfigurator'));
 
         foreach ($this->config['parameters'] as $name => $value) {
             $templateConfigurator->addSetup('addParameter', [$name, $value]);
@@ -47,27 +49,6 @@ class TemplateFactoryExtension extends Nette\DI\CompilerExtension
         foreach ($this->config['filters'] as $name => $filter) {
             $templateConfigurator->addSetup('addFilter', [$name, $filter]);
         }
-    }
-
-    /**
-     * Make sure that LatteExtension is loaded before us and return its TemplateFactory definition.
-     *
-     * @return Nette\DI\ServiceDefinition
-     */
-    public function getNetteTemplateFactory(): Nette\DI\ServiceDefinition
-    {
-        $latteExtension = $this->compiler->getExtensions(Nette\Bridges\ApplicationDI\LatteExtension::class);
-        if ($latteExtension === []) {
-            throw new \LogicException('LatteExtension not found, did you register it in your configuration?');
-        }
-
-        $container = $this->getContainerBuilder();
-        $templateFactory = reset($latteExtension)->prefix('templateFactory');
-        if (!$container->hasDefinition($templateFactory)) {
-            throw new \LogicException('TemplateFactory service from LatteExtension not found. Make sure LatteExtension is loaded before TemplateFactoryExtension.');
-        }
-
-        return $container->getDefinition($templateFactory);
     }
 
 }
